af3a724be628d668ec74a707776f8d617352e4e5
gagangarg123 <garggagan700@gmail.com>
2024-03-15 20:17:35 +0530

    to check it is a even or odd number in java

---
+++ Even.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Even {
+    
+    public static void main(String args[])
+    {
+    System.out.println("Enter any number?");
+        Scanner gagan=new Scanner(System.in);
+        int n=gagan.nextInt();
+        if(n%2==0)
+        {
+             System.out.print("Even number!");
+
+        }
+        else{
+             System.out.print("Odd number!");
+        }
+
+    
+    }
+    
+}
